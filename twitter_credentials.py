1b3fcd6d20e7b82022a2b55f550a681e0e95227f
Ayush Singh <71220959+ayush12singh@users.noreply.github.com>
2021-10-20 01:43:41 +0530

    Actual commit

---
+++ twitter_credentials.py
@@ -0,0 +1,6 @@
+'''Use your own tweepy credentials'''
+access_token_secret="47R3jiQagm3PI3eM8cNj8uUTkUXCJZS7lqBr5mNWpCnr6"
+access_token="1447854807604088838-2Q5ubabVEUI50wBu7hQMIfFZBEEZcq"
+bearer_token="AAAAAAAAAAAAAAAAAAAAADCXUgEAAAAA0WBGCWC1LYF%2BAsSLNVEL9CCSEyg%3DLKhSJDYB1LmgG5TvWBCa7EjxHSm8atazaE7oc880RwFnKYZNl4"
+Api_key="Iqq7EVhJ4VYesoheaUIYKyCoD"
+Api_key_secret="ZMzpkxizXAr0oi1taYZzFttHrEqcsfnH0JcTA9t1gztKif7X8W"
